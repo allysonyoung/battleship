adad1f5dc37d687d1b4795e45b67999e9a488457
allysonyoung <ayoung1009@gmail.com>
2017-12-05 01:05:59 -0500

    increase socket timeout

--- lib/battleship_web/channels/user_socket.ex
+++ lib/battleship_web/channels/user_socket.ex
@@ -7,7 +7,8 @@ defmodule BattleshipWeb.UserSocket do
   channel "player:*", BattleshipWeb.PlayerChannel
   
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, 
+     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
